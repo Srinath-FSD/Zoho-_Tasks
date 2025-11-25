09e06a53fb65d72fcc6176805c01f159a1b547e9
Srinath-FSD <srinath35fsd@gmail.com>
2025-11-25 20:45:10 +0530

    25.11.2025 Task

---
+++ 25-11-2025_Task/actors.js
@@ -0,0 +1,5 @@
+const actors = {NTR : {Name : "NTR", Movies:[{movieName: "DEVARA",Released : 2024},{movieName : "WAR 2", Realeased : 2025} ]},
+    AA : {Name: "Allu Arjun", Movies : [{movieName : "Pushpa the rise",Released : 2021},{movieName: "Pushpa 2 the rule",Released : 2024}]}};
+
+    console.log(actors.NTR);
+    
---
+++ 25-11-2025_Task/directors.js
@@ -0,0 +1,23 @@
+ const directors = {rajamouli: {name: "S. S. Rajamouli",movies: [
+      { movieName: "Baahubali: The Beginning", year: 2015 },
+      { movieName: "Baahubali: The Conclusion", year: 2017 },
+      { movieName: "RRR", year: 2022 }
+    ]
+  },
+
+  lokeshKanagaraj: {name: "Lokesh Kanagaraj",movies: [
+      { movieName: "Kaithi", year: 2019 },
+      { movieName: "Master", year: 2021 },
+      { movieName: "Vikram", year: 2022 }
+    ]
+  },
+
+  atlee: {name: "Atlee",movies: [
+      { movieName: "Raja Rani", year: 2013 },
+      { movieName: "Theri", year: 2016 },
+      { movieName: "Jawan", year: 2023 }
+    ]
+  }
+};
+
+console.log(directors.lokeshKanagaraj.movies);
